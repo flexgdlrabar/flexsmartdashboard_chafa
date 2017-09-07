b375f802a5738b93ed84d1b654f9db5d0080a979
Roberto Abarca <roberto.abarca@flex.com>
2017-09-07 13:35:07 -0700

    [Studio] Modified screen "Dashboard"

--- client/controllers/app/dashboard.js
+++ client/controllers/app/dashboard.js
@@ -2,5 +2,4 @@ angular.module('app').controller('app_dashboard', app_dashboard);
 function app_dashboard($scope, app) {
     'use strict';
     app.init($scope);
-    data = JSON.parse('{"Site":"Test", "Building": "04"}');
 }
